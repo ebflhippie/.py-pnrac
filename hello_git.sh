2323619e18cd351864b4c284d52fc70cc7adfef3
e.b <ebflhippie@gmail.com>
2018-04-15 18:00:21 -0400

    added hello script to repository.

---
+++ hello_git.sh
@@ -0,0 +1,15 @@
+## simple standard script to test git hub repo
+
+## Begin with the shebang 
+
+#!/bin/bash
+
+## adress user and get user input
+
+echo "Welcome to git-hub"
+echo "What is your name?"
+read urnme 
+
+##adress user with inormation and welcome them to this repository.
+
+printf "Hello $urnme.\n This is my practice repository. \n I set this repository up as I am at this point just learning to script, and use git-hub.\n Hopefully it may be useful to others as well in the future."
